7bf76a2dc4d8414f47caf6075e441a4aa7c19cf4
Rafael Amaral Fabris <cc10005060966@LAB.CCT.UENP.EDU.BR>
2019-06-13 11:09:49 -0300

    Add project files.

---
+++ App1/App1/Cadastro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Cadastro : ContentPage
+	{
+		public Cadastro ()
+		{
+			InitializeComponent ();
+		}
+        
+        public async void VoltarTelaLogin(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+	}
+}
---
+++ App1/App1/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        public async void MudarPaginaInicial(object sender, EventArgs args)
+        {
+            if (CampoLogin.Text == "Rafael" && CampoSenha.Text == "1234")
+            {
+                await Navigation.PushModalAsync(new PaginaInicial());
+            }
+            else if (CampoLogin.Text == "Bruno" && CampoSenha.Text == "1234")
+            {
+                await Navigation.PushModalAsync(new PaginaInicial());
+            }
+            else if (CampoLogin.Text == "Vitor" && CampoSenha.Text == "1234")
+            {
+                await Navigation.PushModalAsync(new PaginaInicial());
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Usuário ou Senha incorretos", "OK");
+            }
+        }
+
+        
+	}
+}
