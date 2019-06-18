82b1a7a3dcb0650382b8a786bfe08ce58a20f66b
Rafael Amaral Fabris <CC10005060966@lab.cct.uenp.edu.br>
2019-06-18 09:46:56 -0300

    Add project files.

---
+++ App1/App1/PaginaPrincipal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaPrincipal : TabbedPage
+    {
+        public PaginaPrincipal ()
+        {
+            InitializeComponent();
+        }
+
+        public async void MudarTelaNovoItem(object sender, EventArgs args)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new AdicionarItem()));
+        }
+    }
+}
